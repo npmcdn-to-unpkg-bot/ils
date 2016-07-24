2569e5e36ea902dc3d410803c89e7d90bf7d8a4a
self_refactor <deyan8284@gmail.com>
2016-07-24 23:32:38 +0000

    update

--- _inc/scrapedParse.js
+++ _inc/scrapedParse.js
@@ -3,7 +3,7 @@ const R = require("ramda")
 
 function pluckFn(prop, obj) {
     return R.compose(R.flatten, R.filter(val=>val !== false), R.values, R.mapObjIndexed((val, key)=>{
-        if (key.includes(prop)) {
+        if (key.includes(prop)&&val) {
             return val
         } else {return false}
     }))(obj)
@@ -13,8 +13,19 @@ function sortFn(prop, obj) {
         return R.prop(prop, a).length - R.prop(prop, b).length
     }))(obj)
 }
+function uniq(arr,prop){
+    let willReturn = []
+    return R.compose(R.sort((a,b)=>b.dePart.length-a.dePart.length),R.filter(val=>{
+        if(R.indexOf(val[prop], willReturn)&&val[prop].length>2){
+            willReturn.push(val[prop])
+            return true
+        }else{return false}
+    }),R.map(val=>{
+        return R.merge(val,{dePart: R.replace(/[0-9]/,"",val.dePart)})
+    }))(arr)
+}
 
-function main(translated) {
+function main(word,translated) {
     let willSave = {}
     willSave.synonym = []
     willSave.synonymTranslated = []
@@ -23,11 +34,11 @@ function main(translated) {
     let deEnArr = pluckFn("deEn", translated)
     let synonymArr = pluckFn("synonym", translated)
     let phraseArr = pluckFn("phrase", translated)
-    willSave.deEn = {dePart: `${deEnArr[ 0 ].dePart}`,
+    willSave.deEn = {dePart: word,
 enPart: R.compose(R.join(","), R.uniq, R.sort((a, b)=>a.length - b.length), R.pluck("enPart"))(deEnArr)}
 
     R.map(val =>{
-        if (val.enPart && val.enPart.length > 0) {
+        if (val&&val.enPart && val.enPart.length > 0) {
             if (val.dePart.length > 30) {
                 willSave.phraseTranslated.push(val)
             } else {
@@ -43,16 +54,16 @@ enPart: R.compose(R.join(","), R.uniq, R.sort((a, b)=>a.length - b.length), R.pl
     }, synonymArr)
 
     R.map(val =>{
-        if (val.enPart && val.enPart.length > 0) {
+        if (val&&val.enPart && val.enPart.length > 0) {
             willSave.phraseTranslated.push(val)
         } else {
             willSave.phrase.push(val)
         }
     }, phraseArr)
-    willSave.synonym = sortFn("dePart", willSave.synonym)
-    willSave.synonymTranslated = sortFn("dePart", willSave.synonymTranslated)
-    willSave.phrase = sortFn("dePart", willSave.phrase)
-    willSave.phraseTranslated = sortFn("dePart", willSave.phraseTranslated)
+    willSave.synonym = uniq(willSave.synonym, "dePart")
+    willSave.synonymTranslated = uniq(willSave.synonymTranslated,"dePart")
+    willSave.phrase = uniq(willSave.phrase,"dePart")
+    willSave.phraseTranslated = uniq(willSave.phraseTranslated,"dePart")
     return willSave
 }
 module.exports.main = main
--- _inc/translateBulk.js
+++ _inc/translateBulk.js
@@ -1,26 +1,30 @@
 "use strict"
-const J = require("./common")
+const J = require("../common")
 const async = require("async")
 const R = require("ramda")
 const fs = require("fs-extra")
 const translate = require("../admin/_inc/translate.js")
 const scrapedParse = require("./scrapedParse.js")
 let words = require("./words")
-let arr = R.splitEvery(100, words)
-let willSave = {}
+let arr = R.splitEvery(50, words)
+
 let index = 0
 let willMap = words.map(val=>{
+//let willMap = arr[index].map(val=>{
     return function(callback) {
         J.log(val)
         translate.deEnTimer(val).then(incoming=>{
-            callback(null)
-            willSave[ val ] = scrapedParse.main(incoming)
+            callback(null,{word:val, data:incoming})
         })
     }
 })
 async.series(willMap,
 function(err, results) {
-    fs.writeJson(`data${index}.json`, willSave, ()=>{
+    let willSave = {}
+    results.map(val=>{
+        willSave[val.word]=scrapedParse.main(val.word,val.data)
+    })
+    fs.writeJson(`dataFile.json`, willSave, ()=>{
         J.log(1)
     })
 })
