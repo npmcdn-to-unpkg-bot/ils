9e15cdf8d49c3598a8a029f22de51219fec88b37
Dejan Toteff <deyan8284@gmail.com>
2016-07-26 04:00:05 +0300

    update

--- admin/ilsApp.js
+++ admin/ilsApp.js
@@ -6,10 +6,19 @@ const favicon = require("serve-favicon")
 const bodyParser = require("body-parser")
 const mainRoute = require("./routes/indexAlt.js")
 let app = express()
+app.set("views", __dirname + "/views")
+app.set("view engine", "jsx")
+app.engine("jsx", require("express-react-views").createEngine())
+app.use(favicon(__dirname + "/public/favicon.ico"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
 app.use((req, res, next) =>{
     if (!J.auth(req.ip)) {
         J.log(req.ip)
         res.send("No")
+    }else{
+        J.log("ok")
     }
     if (req.body) {
         J.logger.debug(`${req.url} ${req.body}`)
@@ -21,13 +30,6 @@ app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
-app.set("views", __dirname + "/views")
-app.set("view engine", "jsx")
-app.engine("jsx", require("express-react-views").createEngine())
-app.use(favicon(__dirname + "/public/favicon.ico"))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, "public")))
 app.use("/", mainRoute)
 app.use(function (req, res) {
     res.send({
