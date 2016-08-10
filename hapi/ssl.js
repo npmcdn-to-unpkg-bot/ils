1fc0b6682895d1538dfbfdc7fd1edff179cb3ec5
Dejan Toteff <deyan8284@gmail.com>
2016-08-10 23:56:38 +0300

    Wed, 10 Aug 2016 20:56:38 GMT

--- hapi/ssl.js
+++ hapi/ssl.js
@@ -1,38 +1,26 @@
-var express = require('express'),
-    http = require('http'),
-    https = require('https'),
-    fs = require('fs'),
-    path = require('path'),
-    httpApp = express(),
-    app = express()
+#!/usr/bin/env node
 
-
+"use strict"
+const app = require("./app.js")
+const express = require('express')
+const http = require("http")
+const https = require("https")
+let httpApp = express()
 var httpsOptions = {
     key: fs.readFileSync("/etc/letsencrypt/live/ilearnsmarter.com/privkey.pem"),
     cert: fs.readFileSync("/etc/letsencrypt/live/ilearnsmarter.com/fullchain.pem")
 }
-httpApp.set('port', 3001);
+const port = 3000
+httpApp.set('port', port);
 httpApp.get("*", function (req, res, next) {
     res.redirect("https://" + req.headers.host + "/" + req.path);
-});
-
-app.set('port', 443);
-app.enable('trust proxy');
-
-
-app.get('/', (req,res)=>{
-    res.send("done")
-});
-
-app.get('/test', (req,res)=>{
-    res.send("done")
-});
-
-
+})
+app.set("port", 443)
+app.enable('trust proxy')
 http.createServer(httpApp).listen(httpApp.get('port'), function() {
     console.log('Express HTTP server listening on port ' + httpApp.get('port'));
 });
 
 https.createServer(httpsOptions, app).listen(app.get('port'), function() {
     console.log('Express HTTPS server listening on port ' + app.get('port'));
-});
+})
