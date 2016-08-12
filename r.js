799c6afee9e363a780a60000af6987a2d76621cc
Dejan Toteff <deyan8284@gmail.com>
2016-08-12 23:51:11 +0300

    Fri, 12 Aug 2016 20:51:11 GMT-hhHWfBXa62

--- r.js
+++ r.js
@@ -94,15 +94,11 @@ function* summer() {
     return true
 }
 generator = summer()
-
 //proceed until the first "yield" expression, ignoring the "value" argument
 generator.next()
-
 //from this point on, the generator aggregates values until we send "null"
 generator.next(1)
 generator.next(10)
 generator.next(100)
-
 //close the generator and collect the result
 let sum = generator.next(null)
-J.lg(sum)
