fdc163999f04a3293093388f88ad817c32e170e6
Dejan Toteff <deyan8284@gmail.com>
2016-08-13 11:49:02 +0300

    Sat, 13 Aug 2016 08:49:02 GMT-WKJWnHjg5y

--- hapi/views/learningMemeAdmin.jsx
+++ hapi/views/learningMemeAdmin.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react"
 const DefaultLayout = require("./base/learningMemeAdmin.jsx")
 let App = React.createClass({
     render: function() {
-        return <DefaultLayout title="translateDraft" keyword="translateDraft"></DefaultLayout>
+        return <DefaultLayout title="learningMemeAdmin" keyword="learningMemeAdmin"></DefaultLayout>
     }
 })
 module.exports = App
