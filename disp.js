1601b6013d88028aad39b5614563c1e9033f966e
Dejan Toteff <deyan8284@gmail.com>
2016-08-22 07:42:57 +0300

    Mon, 22 Aug 2016 04:42:57 GMT-Q2D3Vwq2nA

---
+++ disp.js
@@ -0,0 +1,51 @@
+"use strict"
+const J = require("justdo")
+const R = require("ramda")
+const argv = require("minimist")(process.argv.slice(2))
+const backup = require("./services/backup/main")
+const spdyClean = require("./zSpdyClean")
+const nvm = require("./zNvm")
+const translateDraftGenerator = require("./_inc/translateDraftGenerator")
+//$ node example/parse.js -a beep -b boop
+//{ _: [], a: 'beep', b: 'boop' }
+let commandArgument
+function main(command) {
+    return new Promise(resolve=>{
+        if (command === "backup") {
+            backup.main().then(data=>{
+                resolve(data)
+            })
+        }
+        if (command === "translateDraft") {
+            translateDraftGenerator.alt().then(data=>{
+                resolve(data)
+            })
+        }
+        if (command === "spdyClean") {
+            spdyClean.main().then(()=>{
+                resolve(true)
+            })
+        }
+        if (command === "nvm") {
+            nvm.main(commandArgument).then(()=>{
+                resolve(true)
+            })
+        }
+        if (command === "nvmAlt") {
+            nvm.alt(commandArgument).then(()=>{
+                resolve(true)
+            })
+        }
+    })
+}
+if (argv._.length === 1) {
+    main(argv._[ 0 ]).then(data=>{
+        J.log(data)
+    })
+}
+if (argv._.length === 2) {
+    commandArgument = argv._[ 1 ]
+    main(argv._[ 0 ]).then(data=>{
+        J.log(data)
+    })
+}
